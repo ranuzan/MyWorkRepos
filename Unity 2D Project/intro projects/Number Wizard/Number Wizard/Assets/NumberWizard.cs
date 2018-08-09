36dbf8180e3a28069637bcead1e03948e94c2040
ranuzan <ranuzan1@gmail.com>
2018-08-09 12:42:40 +0300

    finished number wizard

---
+++ Unity 2D Project/intro projects/Number Wizard/Number Wizard/Assets/NumberWizard.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*this simple game guesses your number in a binary search way and prints it to console*/
+public class NumberWizard : MonoBehaviour {
+    int min = 1;
+    int max = 1000;
+    int guess = 500;
+    // Use this for initialization
+    void Start () {
+        min = 1;
+        max = 1000;
+        guess = (max + min) / 2;
+
+        Debug.Log("pick a num between "+min+ " to "+max);
+        Debug.Log("current guess is " + guess);
+        Debug.Log("up = higher -   down = lower  -  enter = correct");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            guess = (max + min) / 2;
+            Debug.Log("current guess is " + guess);
+            Debug.Log("up = higher -   down = lower  -  enter = correct");
+            if (guess == 999)
+            {
+                max += 1;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            guess = (max + min) / 2;
+            Debug.Log("current guess is " + guess);
+            Debug.Log("up = higher -   down = lower  -  enter = correct");
+        }
+        else if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("your number is " + guess);
+        }
+    }
+}
