bff03d903aae49a70b4b9dae665d2d605bc900b8
ranuzan <ranuzan1@gmail.com>
2018-08-11 14:18:17 +0300

    finished text game

---
+++ Unity 2D Project/intro projects/Text101/Text101/Assets/Scripts/AdventureGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    [SerializeField] State StartingState;
+
+    State state;
+	// Use this for initialization
+	void Start () {
+        state = StartingState;
+        textComponent.text = state.getStateStory();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManageState();
+	}
+
+    private void ManageState()
+    {
+        var nextStates = state.getNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+        textComponent.text = state.getStateStory();
+    }
+}
