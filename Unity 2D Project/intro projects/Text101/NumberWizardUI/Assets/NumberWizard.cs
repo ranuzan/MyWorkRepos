9fa90d9f371d73c80481419e13b3817a8b0ab1fe
ranuzan <ranuzan1@gmail.com>
2018-08-17 16:35:19 +0300

    finished num wiz ui

---
+++ Unity 2D Project/intro projects/Text101/NumberWizardUI/Assets/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+/*this simple game guesses your number in a binary search way and prints it to console*/
+public class NumberWizard : MonoBehaviour {
+    [SerializeField] int min;
+    [SerializeField] int max;
+    [SerializeField] int guess;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    // Use this for initialization
+    void Start () {
+        nextguess();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+    public void onPressHiger()
+    {
+        min = guess+1;
+        nextguess();
+        return;
+    }
+    public void onPressLower()
+    {
+        max = guess+1;
+        nextguess();
+        return;
+    }
+    public void nextguess()
+    {
+        guess = Random.Range(min,max+1); ;
+        guessText.text = (guess.ToString());
+    }
+
+}
